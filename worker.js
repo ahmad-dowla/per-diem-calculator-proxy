57c64a82551a0056f6145a51156a6cd189c8d318
ahmad-dowla <ahmad.dowla@gmail.com>
2025-04-24 12:25:51 -0500

    Adjust variable names.

--- worker.js
+++ worker.js
@@ -6,45 +6,13 @@ export default {
       "Access-Control-Max-Age": "86400",
     };
 
-    async function handleRequest(request) {
-      const url = new URL(request.url);
-      let apiUrl = url.searchParams.get("url");
-
-      if (apiUrl === null) {
-        throw new Error("URL missing.");
-      }
-
-      if (request.headers.get("x-perdiem-key") !== env.PROXY_KEY) {
-        throw new Error("Invalid API key.");
-      }
-
-      // Rewrite request to point to API URL. This also makes the request mutable
-      // so you can add the correct Origin header to make the API server think
-      // that this request is not cross-site.
-      request = new Request(apiUrl, request);
-      request.headers.set("x-perdiem-key", null);
-      apiUrl.includes("https://api.gsa.gov") &&
-        request.headers.set("x-api-key", env.GSA_KEY);
-      request.headers.set("Origin", new URL(apiUrl).origin);
-      let response = await fetch(request);
-      // Recreate the response so you can modify the headers
-
-      response = new Response(response.body, response);
-      // Set CORS headers
-
-      response.headers.set("Access-Control-Allow-Origin", "*");
-
-      // Append to/Add Vary header so browser will cache response correctly
-      response.headers.append("Vary", "Origin");
-
-      return response;
-    }
-
     async function handleOptions(request) {
+       // Make sure the necessary headers are present for this to be a valid pre-flight request
+      let headers = request.headers;
       if (
-        request.headers.get("Origin") !== null &&
-        request.headers.get("Access-Control-Request-Method") !== null &&
-        request.headers.get("Access-Control-Request-Headers") !== null
+        headers.get("Origin") !== null &&
+        headers.get("Access-Control-Request-Method") !== null &&
+        headers.get("Access-Control-Request-Headers") !== null
       ) {
         // Handle CORS preflight requests.
         return new Response(null, {
@@ -64,6 +32,38 @@ export default {
         });
       }
     }
+
+    async function handleRequest(request) {
+      const url = new URL(request.url);
+      let apiUrl = url.searchParams.get("url");
+      if (apiUrl === null) {
+        throw new Error("URL missing.");
+      }
+      if (request.headers.get("x-perdiem-key") !== env.PROXY_KEY) {
+        throw new Error("Invalid API key.");
+      }
+
+      // Rewrite request to point to API URL. This also makes the request mutable
+      // so you can add the correct Origin header to make the API server think
+      // that this request is not cross-site.
+      const newRequest = new Request(apiUrl, request);
+      newRequest.headers.set("x-perdiem-key", null);
+      apiUrl.includes("https://api.gsa.gov") && newRequest.headers.set("x-api-key", env.GSA_KEY);
+      newRequest.headers.set("Origin", new URL(apiUrl).origin);
+      let response = await fetch(newRequest);
+      // Recreate the response so you can modify the headers
+
+      response = new Response(response.body, response);
+      // Set CORS headers
+
+      response.headers.set("Access-Control-Allow-Origin", "*");
+
+      // Append to/Add Vary header so browser will cache response correctly
+      response.headers.append("Vary", "Origin");
+
+      return response;
+    }
+
 
     if (request.method === "OPTIONS") {
       // Handle CORS preflight requests
