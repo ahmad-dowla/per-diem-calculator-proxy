5a8a43de8f75f85af804d18559d031dbed877fdb
ahmad-dowla <ahmad.dowla@gmail.com>
2025-04-24 13:06:11 -0500

    N/A

--- worker.js
+++ worker.js
@@ -8,63 +8,78 @@ export default {
 
     async function handleRequest(request) {
       const url = new URL(request.url);
-
       let targetUrl = url.searchParams.get("url");
+
       if (targetUrl === null) {
-        throw new Error("URL missing.");
+        throw new Error('URL missing.');
       }
 
-      if (request.headers.get("x-perdiem-key") !== env.PROXY_KEY) {
-        throw new Error("Invalid API key.");
+      if (request.headers.get('x-perdiem-key') !== env.PROXY_KEY) {
+        throw new Error('Invalid API key.');
       }
 
-      // Rewrite request to point to target URL.
+      // Rewrite request to point to API URL. This also makes the request mutable
+      // so you can add the correct Origin header to make the API server think
+      // that this request is not cross-site.
       const newRequest = new Request(targetUrl, request);
-      newRequest.headers.set("x-perdiem-key", null);
-
-      targetUrl.includes('api.gsa.gov') &&
-        newRequest.headers.set("x-api-key", env.GSA_KEY);
-
-      newRequest.headers.set("Origin", new URL(targetUrl).origin);
-
+      newRequest.headers.set("Origin", new URL(apiUrl).origin);
+      newRequest.headers.set('x-perdiem-key',null);
+      targetUrl.includes('https://api.gsa.gov') && newRequest.headers.set('x-api-key',env.GSA_KEY)
       let response = await fetch(newRequest);
       // Recreate the response so you can modify the headers
+
       response = new Response(response.body, response);
       // Set CORS headers
-      response.headers.set("Access-Control-Allow-Origin", "*");
+
+      response.headers.set("Access-Control-Allow-Origin", url.origin);
+
       // Append to/Add Vary header so browser will cache response correctly
       response.headers.append("Vary", "Origin");
+
       return response;
     }
 
     async function handleOptions(request) {
-      // Handle CORS preflight requests.
-      return new Response(null, {
-        headers: {
-          ...corsHeaders,
-          "Access-Control-Allow-Headers": request.headers.get(
-            "Access-Control-Request-Headers",
-          ),
-        },
-      });
-    }    
-
-
-    if (request.method === "OPTIONS") {
-      // Handle CORS preflight requests
-      return handleOptions(request);
-    } else if (
-      request.method === "GET" ||
-      request.method === "HEAD" ||
-      request.method === "POST"
-    ) {
-      // Handle requests to the API server
-      return handleRequest(request);
-    } else {
-      return new Response(null, {
-        status: 405,
-        statusText: "Method Not Allowed",
-      });
+      if (
+        request.headers.get("Origin") !== null &&
+        request.headers.get("Access-Control-Request-Method") !== null &&
+        request.headers.get("Access-Control-Request-Headers") !== null
+      ) {
+        // Handle CORS preflight requests.
+        return new Response(null, {
+          headers: {
+            ...corsHeaders,
+            "Access-Control-Allow-Headers": request.headers.get(
+              "Access-Control-Request-Headers",
+            ),
+          },
+        });
+      } else {
+        // Handle standard OPTIONS request.
+        return new Response(null, {
+          headers: {
+            Allow: "GET, HEAD, POST, OPTIONS",
+          },
+        });
+      }
     }
+
+      if (request.method === "OPTIONS") {
+        // Handle CORS preflight requests
+        return handleOptions(request);
+      } else if (
+        request.method === "GET" ||
+        request.method === "HEAD" ||
+        request.method === "POST"
+      ) {
+        // Handle requests to the API server
+        return handleRequest(request);
+      } else {
+        return new Response(null, {
+          status: 405,
+          statusText: "Method Not Allowed",
+        });
+      }
+
   },
 };
