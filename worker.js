f2c34fcf1e3dc6e0b4ab46acbfc3c8c56aeac420
ahmad-dowla <ahmad.dowla@gmail.com>
2025-04-30 12:26:56 -0500

    Added content-type header

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ export default {
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-      "Access-Control-Max-Age": "86400",
+      'Access-Control-Allow-Headers': 'Content-Type',      "Access-Control-Max-Age": "86400",
     };
 
     async function handleRequest(request) {
